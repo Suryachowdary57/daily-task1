c864951a104eef5e722dc513f25b7275de3461a3
sasikumar <phanisurya2002@gmail.com>
2024-09-23 16:47:18 +0530

    daily task

---
+++ daily_task.js
@@ -0,0 +1,40 @@
+ 
+    
+        // data types//
+        let name = "surya";
+        let age = 25;
+        let expression = true;
+        let expreiance = null;
+        let salary = null
+        console.log("this is my name",name)
+        console.log( typeof name)
+        console.log("this is my age",age)
+        console.log( typeof age)
+        console.log("this expression is ",expression)
+        console.log(typeof expression)
+        console.log("this is my expreiance",expreiance)
+        console.log( typeof experiance)
+        console.log("this value is",salary)
+        console.log(typeof salary)
+        
+        //mathematical operators//
+        num1 = 10;
+        num2 = 20;
+        console.log(" addition of num1+num2 =",num1+num2)
+        console.log(" sub of num1+num2 = ",num1-num2)
+        console.log(" multiplication of num1*num2 = ",num1*num2)
+        console.log(" division of num1/num2 = ",num1/num2)
+        console.log("square of num1**num2",num1**num2)
+        console.log(" modulus of num1%num2 = ",num1%num2)
+
+        //comparator operators//
+        let a=50;
+        b = 60;
+        console.log(`this the == operator of ${a} and ${b}`,a==b );
+        console.log(`this the === operator of ${a} and ${b}`,a===b );
+        console.log(`this the != operator of ${a} and ${b}`,a!=b );
+        console.log(`this the <= operator of ${a} and ${b}`,a<=b );
+        console.log(`this the >= operator of ${a} and ${b}`,a>=b );
+        console.log(`this the > operator of ${a} and ${b}`,a>b );
+         console.log(` this is the operator of ${a} and ${b},a<b`);
+ 
